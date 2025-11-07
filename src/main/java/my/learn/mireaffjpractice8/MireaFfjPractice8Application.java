8ec876ce44ebf4c9ebcc48135d67bcda99f1ec9e
Sun1tAr <denchik2376@yandex.ru>
2025-11-07 18:29:12 +0300

    Initial commit

---
+++ src/main/java/my/learn/mireaffjpractice8/MireaFfjPractice8Application.java
@@ -0,0 +1,13 @@
+package my.learn.mireaffjpractice8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MireaFfjPractice8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MireaFfjPractice8Application.class, args);
+	}
+
+}
